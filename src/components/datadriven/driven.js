4f8f9e51e2030f1ae6d59b44692fa3e281a7a3e1
hernan <hernan09.gz@gmail.com>
2022-08-24 16:03:52 -0300

    commit 2

--- src/components/datadriven/driven.js
+++ src/components/datadriven/driven.js
@@ -8,63 +8,67 @@ function Driven({ data, loader }) {
   const classPokemon = [
     {
       pokename: 'Grass',
-      icon: 'https://static.wikia.nocookie.net/pokemongo/images/0/0a/Icon_Grass.png/revision/latest/scale-to-width-down/25?cb=20171219195826',
+      icon: 'https://www.kindpng.com/picc/m/316-3164678_pokemon-symbol-png-transparent-png.png',
     },
     {
       pokename: 'Bug',
-      icon: 'https://static.wikia.nocookie.net/pokemongo/images/8/88/Icon_Bug.png/revision/latest/scale-to-width-down/25?cb=20171219195822',
+      icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Pok%C3%A9mon_Bug_Type_Icon.svg/240px-Pok%C3%A9mon_Bug_Type_Icon.svg.png',
     },
     {
       pokename: 'Lightning',
-      icon: 'https://static.wikia.nocookie.net/pokemongo/images/1/1c/Icon_Electric.png/revision/latest/scale-to-width-down/25?cb=20171219195824',
+      icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH1elmnoXfefk-78O12__EuJri5RY4G8zh5A&usqp=CAU',
     },
     {
       pokename: 'Dragon',
-      icon: 'https://static.wikia.nocookie.net/pokemongo/images/d/d4/Icon_Dragon.png/revision/latest/scale-to-width-down/25?cb=20171219195823',
+      icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Pok%C3%A9mon_Dragon_Type_Icon.svg/1200px-Pok%C3%A9mon_Dragon_Type_Icon.svg.png',
     },
     {
       pokename: 'Water',
-      icon: 'https://static.wikia.nocookie.net/pokemongo/images/6/65/Icon_Water.png/revision/latest/scale-to-width-down/25?cb=20171219195830',
+      icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Pok%C3%A9mon_Water_Type_Icon.svg/2048px-Pok%C3%A9mon_Water_Type_Icon.svg.png',
     },
     {
       pokename: 'Darkness',
-      icon: 'https://static.wikia.nocookie.net/pokemongo/images/e/e9/Icon_Dark.png/revision/latest/scale-to-width-down/25?cb=20171219195823',
+      icon: 'https://pngset.com/images/the-strongest-attacks-for-each-type-in-pokmon-go-dark-type-pokemon-logo-symbol-trademark-moon-outer-space-transparent-png-2603328.png',
     },
     {
       pokename: 'Metal',
-      icon: 'https://static.wikia.nocookie.net/pokemongo/images/3/38/Icon_Steel.png/revision/latest/scale-to-width-down/25?cb=20171219195830',
+      icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjSGUJSwGW0wDJHT-SLoLqKF-Q8_7gziDt6hxQwpBprMJnTajKknx-78p2SoIkLG8FkZ4&usqp=CAU',
     },
     {
       pokename: 'Colorless',
-      icon: 'https://static.wikia.nocookie.net/pokemongo/images/4/43/Icon_Normal.png/revision/latest/scale-to-width-down/25?cb=20171219195828',
+      icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Pok%C3%A9mon_Normal_Type_Icon.svg/1200px-Pok%C3%A9mon_Normal_Type_Icon.svg.png',
     },
     {
       pokename: 'Psychic',
-      icon: 'https://static.wikia.nocookie.net/pokemongo/images/c/ce/Icon_Psychic.png/revision/latest/scale-to-width-down/25?cb=20171219195829',
+      icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnZY_wptqdjEv7LsgH73FeKqhJzr4oT5ultA&usqp=CAU',
     },
     {
       pokename: 'Fire',
-      icon: 'https://static.wikia.nocookie.net/pokemongo/images/0/0a/Icon_Fire.png/revision/latest/scale-to-width-down/25?cb=20171219195825',
+      icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Pok%C3%A9mon_Fire_Type_Icon.svg/1200px-Pok%C3%A9mon_Fire_Type_Icon.svg.png',
     },
     {
       pokename: 'Fighting',
-      icon: 'https://static.wikia.nocookie.net/pokemongo/images/f/f0/Icon_Fighting.png/revision/latest?cb=20171219195825',
+      icon: 'https://e7.pngegg.com/pngimages/374/734/png-clipart-pokemon-types-pokemon-trading-card-game-video-symbol-blue-fire-dragon-orange-logo.png',
     },
     {
       pokename: 'Ice',
-      icon: 'https://static.wikia.nocookie.net/pokemongo/images/5/52/Icon_Ice.png/revision/latest/scale-to-width-down/25?cb=20171219195828',
+      icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlvBN1h83YXcTGPoSzRhE_SnFly5n-OX5dKg&usqp=CAU',
     },
     {
       pokename: 'Ghost',
-      icon: 'https://static.wikia.nocookie.net/pokemongo/images/7/7d/Icon_Ghost.png/revision/latest/scale-to-width-down/25?cb=20171219195826',
+      icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Pok%C3%A9mon_Ghost_Type_Icon.svg/800px-Pok%C3%A9mon_Ghost_Type_Icon.svg.png',
     },
     {
       pokename: 'Fairy',
-      icon: 'https://static.wikia.nocookie.net/pokemongo/images/7/7f/Icon_Fairy.png/revision/latest/scale-to-width-down/25?cb=20171219195824',
+      icon: 'https://logodix.com/logo/565521.jpg',
     },
     {
       pokename: 'Ground',
-      icon: 'https://static.wikia.nocookie.net/pokemongo/images/7/71/Icon_Ground.png/revision/latest/scale-to-width-down/25?cb=20171219195827',
+      icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS39NTuVG7XbrLOrXa05UKyNCk_PljW_Dra7w&usqp=CAU',
+    },
+    {
+      pokename: 'Poison',
+      icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Pok%C3%A9mon_Poison_Type_Icon.svg/800px-Pok%C3%A9mon_Poison_Type_Icon.svg.png',
     },
   ];
 
@@ -110,6 +114,7 @@ function Driven({ data, loader }) {
                     key={index}
                     className="list-group-item list-group-item-action hvr-curl-bottom-right"
                   >
+                    <img src={item.icon} className="span-icon" />
                     {item.pokename}
                   </a>
                 </div>
@@ -129,7 +134,7 @@ function Driven({ data, loader }) {
                 ></img>
               )}
               {!loader && (
-                <div className="spinner-grow text-danger" role="status">
+                <div className="spinner-border text-primary" role="status">
                   <span className="visually-hidden">Loading...</span>
                 </div>
               )}
